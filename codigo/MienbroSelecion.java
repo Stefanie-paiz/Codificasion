7ac5646ad7873917749089ca337d6f680bd60657
Your Name <you@example.com>
2025-05-26 20:26:30 -0700

    Se finalizo la clase con el codigo

---
+++ codigo/MienbroSelecion.java
@@ -0,0 +1,24 @@
+public class MienbroSelecion {
+    // Atributos comunes
+    protected int id;
+    protected String nombre;
+    protected String apellidos;
+    protected int edad;
+
+    // Constructor
+    public MienbroSelecion(int id, String nombre, String apellidos, int edad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
+
+    // Métodos comunes
+    public void concentrarse() {
+        System.out.println(nombre + " " + apellidos + " está concentrándose.");
+    }
+
+    public void viajar() {
+        System.out.println(nombre + " " + apellidos + " está viajando.");
+    }
+}
